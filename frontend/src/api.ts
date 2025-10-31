26bb9720c4a2988c90200a19c15b239836512ea5
Mattias Holmgren <me@mattjh.sh>
2025-10-31 21:57:15 +0100

    add client api calls

---
+++ frontend/src/api.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import type { Account } from "./types";
+
+const API_BASE = "/api";
+
+export const getAccounts = async (): Promise<Account[]> => {
+  const res = await axios.get(`${API_BASE}/accounts`);
+  return res.data;
+};
+
+export const createAccount = async (
+  name: string,
+  parent_id?: number | null
+) => {
+  const res = await axios.post(`${API_BASE}/accounts`, { name, parent_id });
+  return res.data;
+};
+
+export const updateBalance = async (id: number, delta: number) => {
+  const res = await axios.patch(`${API_BASE}/accounts/${id}/balance`, { delta });
+  return res.data;
+};
